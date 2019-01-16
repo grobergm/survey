f4044f3e19f8efa0cf11bbbe314b5b297c7f8745
Ian Cook Westgate and Matt Groberg <matt@>
2019-01-16 13:27:04 -0800

    add more survey functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 
 
 $(function() {
-  $("form#superForm").submit(function(event){
-  event.preventDefault();
-  var beverage = $("#beverage").val();
-  console.log(beverage);
-  })
-})
+  $(".superForm").submit(function(event){
+    event.preventDefault();
+
+    var beverage = $("select#beverage").val();
+    $(".beverageChoice").text(beverage);
+
+    var pet = $("input:radio[name=animal]:checked").val();
+    $(".petChoice").text(pet);
+
+    var vacayDay = $("#vacay").val();
+    $(".vacayDay").text(vacayDay);
+
+    var favColor= $("#color").val();
+    console.log(favColor)
+    $("body").css("background-color",favColor);
+
+  });
+});
